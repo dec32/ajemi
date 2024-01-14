75650a08eb71c32beab12045a677fb052bd39f90
Dec_32 <1064103102@qq.com>
2024-01-14 21:38:17 +0800

    Fixed the issue where the IME will randomly stop recognizing prefixes

--- src/dict.rs
+++ src/dict.rs
@@ -1,51 +1,52 @@
-use std::{collections::HashMap, ffi::OsStr, cell::OnceCell, io::Read, any::Any, fmt::Debug};
+use std::{collections::HashMap, ffi::OsStr, cell::OnceCell};
 
 use crate::extend::OsStrExt2;
-use Suggestion::*;
 use log::debug;
 
 /// A private struct to store and query words.
 struct Dict {
-    // FIXME: the map can be extremly big (O(2^n)), a string pool may help
-    suggestions: HashMap<Vec<u16>, Suggestion>
-}
-
-enum Suggestion {
-    Exact(Vec<u16>),
-    Unique(Vec<u16>),
-    Duplicate,
-}
-
-impl Debug for Suggestion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Exact(arg0) => f.debug_tuple("Exact").field(&String::from_utf16_lossy(arg0)).finish(),
-            Self::Unique(arg0) => f.debug_tuple("Unique").field(&String::from_utf16_lossy(arg0)).finish(),
-            Self::Duplicate => write!(f, "Duplicate"),
-        }
-    }
+    suggestions: HashMap<Vec<u16>, Vec<u16>>,
 }
 
 impl Dict {
-    fn new() -> Dict {
-        Dict{suggestions: HashMap::new()}
-    }
-    fn insert(&mut self, spelling: &str, word: &str) {
-        let spelling = OsStr::new(spelling).wchars();
-        let word = OsStr::new(word).wchars();
-
-        self.suggestions.insert(spelling.clone(), Exact(word.clone()));
-        for len in 1..spelling.len() {
-            let prefix = &spelling[0..len];
-            match self.suggestions.get(prefix) {
-                None => 
-                    self.suggestions.insert(prefix.to_vec(), Unique(word.clone())),
-                Some(Unique(_)) | Some(Duplicate) => 
-                    self.suggestions.insert(prefix.to_vec(), Duplicate),
-                Some(Exact(_)) 
-                    => None,
+    fn from_entries(entries: Vec<(&str, &str)>) -> Dict {
+        use Candidate::*;
+        enum Candidate {
+            Exact(Vec<u16>),
+            Unique(Vec<u16>),
+            Duplicate,
+        }
+        let mut candidates = HashMap::new();
+        for entry in entries {
+            let spelling = OsStr::new(entry.0).wchars();
+            let word = OsStr::new(entry.1).wchars();
+            candidates.insert(spelling.clone(), Exact(word.clone()));
+            for len in 1..spelling.len() {
+                let prefix = &spelling[0..len];
+                match candidates.get(prefix) {
+                    None => 
+                        candidates.insert(prefix.to_vec(), Unique(word.clone())),
+                    Some(Unique(_)) | Some(Duplicate) => 
+                        candidates.insert(prefix.to_vec(), Duplicate),
+                    Some(Exact(_)) 
+                        => None,
+                };
+            }
+        }
+        // by doing so, we lost the info about whether an ascii sequence is an exact spelling
+        // or a unique prefix. i tried keeping the info, but then the suggest method would 
+        // randomly stop recognizing prefixes.
+        // haven't figured out why and possbilly will never do. i don't have time
+        let mut suggestions = HashMap::new();
+        for (prefix_or_spelling, candidate) in candidates {
+            match candidate {
+                Exact(word) | Unique(word) => 
+                    suggestions.insert(prefix_or_spelling, word),
+                Duplicate =>
+                    None
             };
         }
+        Dict{suggestions}
     }
 
     fn suggest(&self, letters: &[u16]) -> Vec<u16> {
@@ -53,27 +54,20 @@ impl Dict {
         let mut from = 0;
         let mut to = letters.len();
         let mut result = Vec::new();
-
         while from < to {
             let slice = &letters[from..to];
             let suggestion = self.suggestions.get(slice);
-            debug!("Found \"{:?}\" for \"{}\"", suggestion, String::from_utf16_lossy(slice));
-            match suggestion {
-                Some(Exact(word)) | Some(Unique(word)) => {
-                    result.extend_from_slice(word);
-                    from = to;
-                    to = letters.len();
-                },
-                _ => {
-                    to -= 1;
-                }
+            // to match `Some(Exact(word)) | Some(Unique(word))` will cause the issue mentioned above
+            if let Some(word) = suggestion {
+                result.extend_from_slice(word);
+                from = to;
+                to = letters.len();
+            } else {
+                to -= 1;
             }
         }
         result
     }
-
-
-
 }
 
 //----------------------------------------------------------------------------
@@ -85,153 +79,152 @@ impl Dict {
 static mut DICT: OnceCell<Dict> = OnceCell::new();
 
 pub fn setup() {
-    let dict = unsafe{ 
-        DICT.get_or_init(||Dict::new());
-        DICT.get_mut().unwrap()
+    unsafe { 
+        DICT.get_or_init(||Dict::from_entries(vec![
+            ("a", "󱤀"),      
+            ("akesi", "󱤁"),  
+            ("ala", "󱤂"),    
+            ("alasa", "󱤃"),  
+            ("ale", "󱤄"),    
+            ("anpa", "󱤅"),   
+            ("ante", "󱤆"),   
+            ("anu", "󱤇"),    
+            ("awen", "󱤈"),   
+            ("e", "󱤉"),      
+            ("en", "󱤊"),     
+            ("esun", "󱤋"),   
+            ("ijo", "󱤌"),    
+            ("ike", "󱤍"),    
+            ("ilo", "󱤎"),    
+            ("insa", "󱤏"),   
+            ("jaki", "󱤐"),   
+            ("jan", "󱤑"),    
+            ("jelo", "󱤒"),   
+            ("jo", "󱤓"),     
+            ("kala", "󱤔"),   
+            ("kalama", "󱤕"), 
+            ("kama", "󱤖"),   
+            ("kasi", "󱤗"),   
+            ("ken", "󱤘"),    
+            ("kepeken", "󱤙"),
+            ("kili", "󱤚"),   
+            ("kiwen", "󱤛"),  
+            ("ko", "󱤜"),
+            ("kon", "󱤝"),
+            ("kule", "󱤞"),
+            ("kulupu", "󱤟"),
+            ("kute", "󱤠"),
+            ("la", "󱤡"),
+            ("lape", "󱤢"),
+            ("laso", "󱤣"),
+            ("lawa", "󱤤"),
+            ("len", "󱤥"),
+            ("lete", "󱤦"),
+            ("li", "󱤧"),
+            ("lili", "󱤨"),
+            ("linja", "󱤩"),
+            ("lipu", "󱤪"),
+            ("loje", "󱤫"),
+            ("lon", "󱤬"),
+            ("luka", "󱤭"),
+            ("lukin", "󱤮"),
+            ("lupa", "󱤯"),
+            ("ma", "󱤰"),
+            ("mama", "󱤱"),
+            ("mani", "󱤲"),
+            ("meli", "󱤳"),
+            ("mi", "󱤴"),
+            ("mije", "󱤵"),
+            ("moku", "󱤶"),
+            ("moli", "󱤷"),
+            ("monsi", "󱤸"),
+            ("mu", "󱤹"),
+            ("mun", "󱤺"),
+            ("musi", "󱤻"),
+            ("mute", "󱤼"),
+            ("nanpa", "󱤽"),
+            ("nasa", "󱤾"),
+            ("nasin", "󱤿"),
+            ("nena", "󱥀"),
+            ("ni", "󱥁"),
+            ("nimi", "󱥂"),
+            ("noka", "󱥃"),
+            ("o", "󱥄"),
+            ("olin", "󱥅"),
+            ("ona", "󱥆"),
+            ("open", "󱥇"),
+            ("pakala", "󱥈"),
+            ("pali", "󱥉"),
+            ("palisa", "󱥊"),
+            ("pan", "󱥋"),
+            ("pana", "󱥌"),
+            ("pi", "󱥍"),
+            ("pilin", "󱥎"),
+            ("pimeja", "󱥏"),
+            ("pini", "󱥐"),
+            ("pipi", "󱥑"),
+            ("poka", "󱥒"),
+            ("poki", "󱥓"),
+            ("pona", "󱥔"),
+            ("pu", "󱥕"),
+            ("sama", "󱥖"),
+            ("seli", "󱥗"),
+            ("selo", "󱥘"),
+            ("seme", "󱥙"),
+            ("sewi", "󱥚"),
+            ("sijelo", "󱥛"),
+            ("sike", "󱥜"),
+            ("sin", "󱥝"),
+            ("sina", "󱥞"),
+            ("sinpin", "󱥟"),
+            ("sitelen", "󱥠"),
+            ("sona", "󱥡"),
+            ("soweli", "󱥢"),
+            ("suli", "󱥣"),
+            ("suno", "󱥤"),
+            ("supa", "󱥥"),
+            ("suwi", "󱥦"),
+            ("tan", "󱥧"),
+            ("taso", "󱥨"),
+            ("tawa", "󱥩"),
+            ("telo", "󱥪"),
+            ("tenpo", "󱥫"),
+            ("toki", "󱥬"),
+            ("tomo", "󱥭"),
+            ("tu", "󱥮"),
+            ("unpa", "󱥯"),
+            ("uta", "󱥰"),
+            ("utala", "󱥱"),
+            ("walo", "󱥲"),
+            ("wan", "󱥳"),
+            ("waso", "󱥴"),
+            ("wawa", "󱥵"),
+            ("weka", "󱥶"),
+            ("wile", "󱥷"),
+            ("namako", "󱥸"),
+            ("kin", "󱥹"),
+            ("oko", "󱥺"),
+            ("kipisi", "󱥻"),
+            ("leko", "󱥼"),
+            ("monsuta", "󱥽"),
+            ("tonsi", "󱥾"),
+            ("jasima", "󱥿"),
+            ("kijetesantakalu", "󱦀"),
+            ("soko", "󱦁"),
+            ("meso", "󱦂"),
+            ("epiku", "󱦃"),
+            ("kokosila", "󱦄"),
+            ("lanpan", "󱦅"),
+            ("n", "󱦆"),
+            ("misikeke", "󱦇"),
+            ("ku", "󱦈"),
+            ("pake", "󱦠"),
+            ("apeja", "󱦡"),
+            ("majuna", "󱦢"),
+            ("powe", "󱦣"),
+        ]));
     };
-    dict.insert("a", "󱤀");      
-    dict.insert("akesi", "󱤁");  
-    dict.insert("ala", "󱤂");    
-    dict.insert("alasa", "󱤃");  
-    dict.insert("ale", "󱤄");    
-    dict.insert("anpa", "󱤅");   
-    dict.insert("ante", "󱤆");   
-    dict.insert("anu", "󱤇");    
-    dict.insert("awen", "󱤈");   
-    dict.insert("e", "󱤉");      
-    dict.insert("en", "󱤊");     
-    dict.insert("esun", "󱤋");   
-    dict.insert("ijo", "󱤌");    
-    dict.insert("ike", "󱤍");    
-    dict.insert("ilo", "󱤎");    
-    dict.insert("insa", "󱤏");   
-    dict.insert("jaki", "󱤐");   
-    dict.insert("jan", "󱤑");    
-    dict.insert("jelo", "󱤒");   
-    dict.insert("jo", "󱤓");     
-    dict.insert("kala", "󱤔");   
-    dict.insert("kalama", "󱤕"); 
-    dict.insert("kama", "󱤖");   
-    dict.insert("kasi", "󱤗");   
-    dict.insert("ken", "󱤘");    
-    dict.insert("kepeken", "󱤙");
-    dict.insert("kili", "󱤚");   
-    dict.insert("kiwen", "󱤛");
-    dict.insert("ko", "󱤜");
-    dict.insert("kon", "󱤝");
-    dict.insert("kule", "󱤞");
-    dict.insert("kulupu", "󱤟");
-    dict.insert("kute", "󱤠");
-    dict.insert("la", "󱤡");
-    dict.insert("lape", "󱤢");
-    dict.insert("laso", "󱤣");
-    dict.insert("lawa", "󱤤");
-    dict.insert("len", "󱤥");
-    dict.insert("lete", "󱤦");
-    dict.insert("li", "󱤧");
-    dict.insert("lili", "󱤨");
-    dict.insert("linja", "󱤩");
-    dict.insert("lipu", "󱤪");
-    dict.insert("loje", "󱤫");
-    dict.insert("lon", "󱤬");
-    dict.insert("luka", "󱤭");
-    dict.insert("lukin", "󱤮");
-    dict.insert("lupa", "󱤯");
-    dict.insert("ma", "󱤰");
-    dict.insert("mama", "󱤱");
-    dict.insert("mani", "󱤲");
-    dict.insert("meli", "󱤳");
-    dict.insert("mi", "󱤴");
-    dict.insert("mije", "󱤵");
-    dict.insert("moku", "󱤶");
-    dict.insert("moli", "󱤷");
-    dict.insert("monsi", "󱤸");
-    dict.insert("mu", "󱤹");
-    dict.insert("mun", "󱤺");
-    dict.insert("musi", "󱤻");
-    dict.insert("mute", "󱤼");
-    dict.insert("nanpa", "󱤽");
-    dict.insert("nasa", "󱤾");
-    dict.insert("nasin", "󱤿");
-    dict.insert("nena", "󱥀");
-    dict.insert("ni", "󱥁");
-    dict.insert("nimi", "󱥂");
-    dict.insert("noka", "󱥃");
-    dict.insert("o", "󱥄");
-    dict.insert("olin", "󱥅");
-    dict.insert("ona", "󱥆");
-    dict.insert("open", "󱥇");
-    dict.insert("pakala", "󱥈");
-    dict.insert("pali", "󱥉");
-    dict.insert("palisa", "󱥊");
-    dict.insert("pan", "󱥋");
-    dict.insert("pana", "󱥌");
-    dict.insert("pi", "󱥍");
-    dict.insert("pilin", "󱥎");
-    dict.insert("pimeja", "󱥏");
-    dict.insert("pini", "󱥐");
-    dict.insert("pipi", "󱥑");
-    dict.insert("poka", "󱥒");
-    dict.insert("poki", "󱥓");
-    dict.insert("pona", "󱥔");
-    dict.insert("pu", "󱥕");
-    dict.insert("sama", "󱥖");
-    dict.insert("seli", "󱥗");
-    dict.insert("selo", "󱥘");
-    dict.insert("seme", "󱥙");
-    dict.insert("sewi", "󱥚");
-    dict.insert("sijelo", "󱥛");
-    dict.insert("sike", "󱥜");
-    dict.insert("sin", "󱥝");
-    dict.insert("sina", "󱥞");
-    dict.insert("sinpin", "󱥟");
-    dict.insert("sitelen", "󱥠");
-    dict.insert("sona", "󱥡");
-    dict.insert("soweli", "󱥢");
-    dict.insert("suli", "󱥣");
-    dict.insert("suno", "󱥤");
-    dict.insert("supa", "󱥥");
-    dict.insert("suwi", "󱥦");
-    dict.insert("tan", "󱥧");
-    dict.insert("taso", "󱥨");
-    dict.insert("tawa", "󱥩");
-    dict.insert("telo", "󱥪");
-    dict.insert("tenpo", "󱥫");
-    dict.insert("toki", "󱥬");
-    dict.insert("tomo", "󱥭");
-    dict.insert("tu", "󱥮");
-    dict.insert("unpa", "󱥯");
-    dict.insert("uta", "󱥰");
-    dict.insert("utala", "󱥱");
-    dict.insert("walo", "󱥲");
-    dict.insert("wan", "󱥳");
-    dict.insert("waso", "󱥴");
-    dict.insert("wawa", "󱥵");
-    dict.insert("weka", "󱥶");
-    dict.insert("wile", "󱥷");
-    dict.insert("namako", "󱥸");
-    dict.insert("kin", "󱥹");
-    dict.insert("oko", "󱥺");
-    dict.insert("kipisi", "󱥻");
-    dict.insert("leko", "󱥼");
-    dict.insert("monsuta", "󱥽");
-    dict.insert("tonsi", "󱥾");
-    dict.insert("jasima", "󱥿");
-    dict.insert("kijetesantakalu", "󱦀");
-    dict.insert("soko", "󱦁");
-    dict.insert("meso", "󱦂");
-    dict.insert("epiku", "󱦃");
-    dict.insert("kokosila", "󱦄");
-    dict.insert("lanpan", "󱦅");
-    dict.insert("n", "󱦆");
-    dict.insert("misikeke", "󱦇");
-    dict.insert("ku", "󱦈");
-    dict.insert("pake", "󱦠");
-    dict.insert("apeja", "󱦡");
-    dict.insert("majuna", "󱦢");
-    dict.insert("powe", "󱦣");
 }
-
 
 fn dict() -> &'static Dict {
     unsafe {DICT.get().unwrap()}
@@ -239,6 +232,7 @@ fn dict() -> &'static Dict {
 
 pub fn suggest(letters: &[u16]) -> Vec<u16> {
     dict().suggest(letters)
+    // dict()._suggest(letters)
 }
 
 
@@ -246,32 +240,46 @@ pub fn suggest(letters: &[u16]) -> Vec<u16> {
 fn i_dont_know_now_to_write_macros() {
     use std::fs::File;
     use std::path::Path;
+    use std::io::Read;
     let mut file = File::open(Path::new("C:\\ajemi.dict.yaml")).unwrap();
     let mut text = String::with_capacity(2048);
     file.read_to_string(&mut text).unwrap();
     for line in text.lines() {
         let split: Vec<&str> = line.split('\t').collect();
-        println!("dict.insert(\"{}\", \"{}\");", split[1], split[0]);
+        println!("(\"{}\", \"{}\"),", split[1], split[0]);
     }
 }
 
 #[test]
-pub fn test() {
-    let mut dict = Dict::new();
-    dict.insert("mi", "我");
-    dict.insert("lukin", "看");
-    dict.insert("e", "兮");
-    dict.insert("kijetesantakalu", "狸");
-    dict.insert("nasa", "怪");
-    dict.insert("lape", "眠");
+fn repl() {
+    use std::io::stdin;
+    use std::ffi::OsString;
+    setup();
+    let mut buf = String::new();
+    loop {
+        buf.clear();
+        stdin().read_line(&mut buf).unwrap();
+        let wchars = OsString::from(&buf).wchars();
+        let suggestion = String::from_utf16_lossy(&suggest(&wchars));
+        println!("{}", suggestion);
+    }
+
+}
+
+#[test]
+pub fn test_dict() {
+    let dict = Dict::from_entries(vec![
+        ("mi", "我"),
+        ("lukin", "看"),
+        ("e", "兮"),
+        ("kijetesantakalu", "狸"),
+        ("nasa", "怪"),
+        ("lape", "眠"),
+    ]);
+
     for (letters, suggestion) in &dict.suggestions {
         let letters = String::from_utf16_lossy(&letters);
-        let suggestion = match suggestion {
-            Exact(word) => format!("Exact({})", String::from_utf16_lossy(&word)),
-            Unique(word) => format!("Unique({})", String::from_utf16_lossy(&word)),
-            Duplicate => String::from("Unique")
-        };
-        println!("{letters}={suggestion}");
+        println!("{letters}={:?}", suggestion);
     }
 
     let suggestion = dict.suggest(&OsStr::new("milukinekijenasalol").wchars());
