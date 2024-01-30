50c7f6301768910eac19bde8fe5e38b6e1210d58
Dec_32 <1064103102@qq.com>
2024-01-31 03:33:08 +0800

    Allow pi-like long glyphs to close the previous one to open itself.

--- src/engine/long_glyph.rs
+++ src/engine/long_glyph.rs
@@ -16,41 +16,53 @@ pub fn process_long_glyph(text: &mut String) {
     let mut open = false;
     let mut general_question = None;
     for ch in text.chars() {
+        // insert reverse long glyph for ala and remember the question
         if ch == ALA {
             let Some(mut prev) = output.pop() else {
                 output.push(ch);
                 continue;
             };
+            // ala only closes long glyphs that just begin to form structure like "ken ala ken"
             if prev == START_OF_LONG_GLYGH {
                 open = false;
                 prev = output.pop().unwrap();
             }
             if open {
-                output.push(END_OF_LONG_GLYPH);
-            }
-            general_question = Some(prev);
-            output.push(START_OF_REVERSE_LONG_GLYGH);
-            output.push(prev);
-            output.push(END_OF_LONG_REVERSE_GLYPH);
-            output.push(ch);
-        } else {
-            if let Some(gp) = general_question {
-                if ch == gp {
-                    output.push(START_OF_LONG_GLYGH);
-                    output.push(ch);
-                    output.push(END_OF_LONG_GLYPH);
-                } else {
-                    general_question = None;
-                    output.push(ch);
-                }
-                continue;
-            } else {
+                output.push(prev);
                 output.push(ch);
-                if matches!(ch, AWEN|KEN|KEPEKEN|LON|PI|TAWA) && !open {
-                    output.push(START_OF_LONG_GLYGH);
-                    open = true;
+            } else {
+                general_question = Some(prev);
+                output.push(START_OF_REVERSE_LONG_GLYGH);
+                output.push(prev);
+                output.push(END_OF_LONG_REVERSE_GLYPH);
+                output.push(ch);
+            } 
+        // to see if ch is being asked. if so, insert long glyph
+        } else if let Some(gp) = general_question {
+            general_question = None;
+            if ch == gp {
+                output.push(START_OF_LONG_GLYGH);
+                output.push(ch);
+                output.push(END_OF_LONG_GLYPH);
+            } else {
+                // in this case ala is used for denying but not questioning
+                output.push(ch);
+            }
+        // no question, insert ch then open long glyph if needed
+        } else if matches!(ch, AWEN|KEN|KEPEKEN|LON|PI|TAWA) {
+            // close previous long glyph if needed
+            if open {
+                let prev = output.pop().unwrap();
+                if prev != START_OF_LONG_GLYGH {
+                    output.push(prev);
+                    output.push(END_OF_LONG_GLYPH);
                 }
             }
+            output.push(ch);
+            output.push(START_OF_LONG_GLYGH);
+            open = true;
+        } else {
+            output.push(ch);
         }
     }
     if open {
@@ -60,6 +72,8 @@ pub fn process_long_glyph(text: &mut String) {
             output.push(END_OF_LONG_GLYPH);
         }
     }
-    text.clear();
-    text.push_str(&output);
+    if text.len() != output.len() {
+        text.clear();
+        text.push_str(&output);
+    }
 }
