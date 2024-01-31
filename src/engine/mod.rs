f2c396a8d6397a127383940ed4ff43a0f9c00925
Dec_32 <1064103102@qq.com>
2024-02-01 01:34:41 +0800

    Add weight comparison to the newly added sentence predicting algorithm

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -25,14 +25,6 @@ pub struct Suggestion {
     pub groupping: Vec<usize>,
 }
 
-// Suggestion containing extra infos for sentence predicting
-#[derive(Default, Clone)]
-struct Sentence {
-    output: String,
-    groupping: Vec<usize>,
-    exact_count: u8,
-    unique_count: u8
-}
 
 /// Engine. A struct to store and query words and punctuators
 #[derive(Default)]
@@ -147,9 +139,37 @@ impl Engine {
 }
 
 
+// Suggestion containing extra infos for sentence predicting
+#[derive(Default, Clone)]
+struct Sentence {
+    output: String,
+    groupping: Vec<usize>,
+    exact_len: usize,
+    unique_len: usize,
+}
+
 // something experimental
+#[allow(dead_code)]
 impl Engine {
-    #[allow(dead_code)]
+    fn suggest_sentence(&self, spelling: &str) -> Option<Suggestion>{
+        let mut sents = self.suggest_sentences(spelling);
+        let mut best_sent = None;
+        let mut max_weight = 0.0;
+        while !sents.is_empty() {
+            let sent = sents.pop().unwrap();
+            if sent.unique_len == 0 {
+                best_sent = Some(sent);
+                break;
+            }
+            let weight = sent.exact_len as f64 / sent.unique_len as f64;
+            if weight > max_weight {
+                max_weight = weight;
+                best_sent = Some(sent);
+            }
+        }        
+        best_sent.map(|s|Suggestion{output:s.output, groupping: s.groupping})
+    }
+    
     fn suggest_sentences(&self, spelling: &str) -> Vec<Sentence> {
         let mut sent = Sentence::default();
         let mut sents = Vec::new();
@@ -201,7 +221,7 @@ impl Engine {
         if let Some(exact) = exact {
             sent.output.push_str(&exact);
             sent.groupping.push(exact_to);
-            sent.exact_count += 1;
+            sent.exact_len += exact_to - from;
             self.suggest_sentences_recursive(spelling, exact_to, sent, sents)
         }
         if let Some(unique) = unique {
@@ -212,7 +232,7 @@ impl Engine {
             };
             sent.output.push_str(&unique);
             sent.groupping.push(unique_to);
-            sent.unique_count += 1;
+            sent.unique_len += unique_to - from;
             self.suggest_sentences_recursive(spelling, unique_to, sent, sents)
         }
         if let Some(extra_sent) = extra_sent {
@@ -429,7 +449,6 @@ fn repl() {
     }
 }
 
-
 #[test]
 fn repl_sentence() {
     use std::io::stdin;
@@ -438,12 +457,9 @@ fn repl_sentence() {
     loop {
         buf.clear();
         stdin().read_line(&mut buf).unwrap();
-        let sents = engine().suggest_sentences(&buf);
-        for sent in sents {
+        let sents = engine().suggest_sentence(&buf);
+        for sent in sents.iter() {
             println!("{}", sent.output)
         }
     }
 }
-
-
-
