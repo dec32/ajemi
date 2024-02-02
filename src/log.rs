9242dcd6a4257e0bea72e10c370f20fdfd2a356b
Dec_32 <1064103102@qq.com>
2024-02-02 15:05:49 +0800

    Automatically delete log file that is too big

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, path::PathBuf};
+use std::{env, fs, os::windows::fs::MetadataExt, path::PathBuf};
 use chrono::Local;
 use log::LevelFilter::*;
 
@@ -11,7 +11,7 @@ pub fn setup() {
     let _= _setup();
 }
 
-pub fn _setup() -> Result<(), fern::InitError>{
+fn _setup() -> Result<(), fern::InitError>{
     let path = if let Ok(appdata) = env::var("APPDATA") {
         PathBuf::from(appdata).join("Ajemi")
     } else {
@@ -19,7 +19,11 @@ pub fn _setup() -> Result<(), fern::InitError>{
     };
     fs::create_dir_all(&path)?;
     let path = path.join("log.txt");
-    
+    if let Ok(meta) = fs::metadata(&path) {
+        if meta.file_size() >= 5 * 1024 * 1024 {
+            let _ = fs::remove_file(&path);
+        }
+    }
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
