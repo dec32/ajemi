b093429a034c7010b07a2254bc92d1eb03e18fef
dec32 <1064103102@qq.com>
2024-03-23 21:51:12 +0800

    Add support for multiple schemas.

---
+++ src/engine/schema.rs
@@ -0,0 +1,213 @@
+use std::collections::HashMap;
+use super::Candidate;
+use super::Candidate::*;
+
+pub struct Schema {
+    pub candis: HashMap<String, Candidate>,
+    pub puncts: HashMap<char, char>
+}
+
+fn build_candis(entries: Vec<(&str, &str)>) -> HashMap<String, Candidate> {
+    let mut candi = HashMap::new();
+    for (spelling, word) in entries {
+        // store exact spellings -> words
+        candi.insert(spelling.to_string(), Exact(word.to_string(), Vec::new()));
+        // store prefixes -> words
+        for len in 1..spelling.len() {
+            let prefix = &spelling[0..len];
+            match candi.get_mut(prefix) {
+                None => {
+                    candi.insert(prefix.to_string(), Unique(word.to_string()));
+                },
+                Some(Unique(unique)) => {
+                    let mut duplicates = Vec::new();
+                    duplicates.push(unique.clone());
+                    duplicates.push(word.to_string());
+                    candi.insert(prefix.to_string(), Duplicates(duplicates));
+                },
+                Some(Duplicates(duplicates)) | Some(Exact(_, duplicates)) => {
+                    duplicates.push(word.to_string());
+                }
+            }
+        }
+    }
+    candi
+}
+
+
+pub fn sitelen() -> Schema {
+    let candis = build_candis(vec![
+        ("a", "󱤀"),      
+        ("akesi", "󱤁"),  
+        ("ala", "󱤂"),    
+        ("alasa", "󱤃"),  
+        ("ale", "󱤄"),    
+        ("anpa", "󱤅"),   
+        ("ante", "󱤆"),   
+        ("anu", "󱤇"),    
+        ("awen", "󱤈"),   
+        ("e", "󱤉"),      
+        ("en", "󱤊"),     
+        ("esun", "󱤋"),   
+        ("ijo", "󱤌"),    
+        ("ike", "󱤍"),    
+        ("ilo", "󱤎"),    
+        ("insa", "󱤏"),   
+        ("jaki", "󱤐"),   
+        ("jan", "󱤑"),    
+        ("jelo", "󱤒"),   
+        ("jo", "󱤓"),     
+        ("kala", "󱤔"),   
+        ("kalama", "󱤕"), 
+        ("kama", "󱤖"),   
+        ("kasi", "󱤗"),   
+        ("ken", "󱤘"),    
+        ("kepeken", "󱤙"),
+        ("kili", "󱤚"),   
+        ("kiwen", "󱤛"),  
+        ("ko", "󱤜"),
+        ("kon", "󱤝"),
+        ("kule", "󱤞"),
+        ("kulupu", "󱤟"),
+        ("kute", "󱤠"),
+        ("la", "󱤡"),
+        ("lape", "󱤢"),
+        ("laso", "󱤣"),
+        ("lawa", "󱤤"),
+        ("len", "󱤥"),
+        ("lete", "󱤦"),
+        ("li", "󱤧"),
+        ("lili", "󱤨"),
+        ("linja", "󱤩"),
+        ("lipu", "󱤪"),
+        ("loje", "󱤫"),
+        ("lon", "󱤬"),
+        ("luka", "󱤭"),
+        ("lukin", "󱤮"),
+        ("lupa", "󱤯"),
+        ("ma", "󱤰"),
+        ("mama", "󱤱"),
+        ("mani", "󱤲"),
+        ("meli", "󱤳"),
+        ("mi", "󱤴"),
+        ("mije", "󱤵"),
+        ("moku", "󱤶"),
+        ("moli", "󱤷"),
+        ("monsi", "󱤸"),
+        ("mu", "󱤹"),
+        ("mun", "󱤺"),
+        ("musi", "󱤻"),
+        ("mute", "󱤼"),
+        ("nanpa", "󱤽"),
+        ("nasa", "󱤾"),
+        ("nasin", "󱤿"),
+        ("nena", "󱥀"),
+        ("ni", "󱥁"),
+        ("nimi", "󱥂"),
+        ("noka", "󱥃"),
+        ("o", "󱥄"),
+        ("olin", "󱥅"),
+        ("ona", "󱥆"),
+        ("open", "󱥇"),
+        ("pakala", "󱥈"),
+        ("pali", "󱥉"),
+        ("palisa", "󱥊"),
+        ("pan", "󱥋"),
+        ("pana", "󱥌"),
+        ("pi", "󱥍"),
+        ("pilin", "󱥎"),
+        ("pimeja", "󱥏"),
+        ("pini", "󱥐"),
+        ("pipi", "󱥑"),
+        ("poka", "󱥒"),
+        ("poki", "󱥓"),
+        ("pona", "󱥔"),
+        ("pu", "󱥕"),
+        ("sama", "󱥖"),
+        ("seli", "󱥗"),
+        ("selo", "󱥘"),
+        ("seme", "󱥙"),
+        ("sewi", "󱥚"),
+        ("sijelo", "󱥛"),
+        ("sike", "󱥜"),
+        ("sin", "󱥝"),
+        ("sina", "󱥞"),
+        ("sinpin", "󱥟"),
+        ("sitelen", "󱥠"),
+        ("sona", "󱥡"),
+        ("soweli", "󱥢"),
+        ("suli", "󱥣"),
+        ("suno", "󱥤"),
+        ("supa", "󱥥"),
+        ("suwi", "󱥦"),
+        ("tan", "󱥧"),
+        ("taso", "󱥨"),
+        ("tawa", "󱥩"),
+        ("telo", "󱥪"),
+        ("tenpo", "󱥫"),
+        ("toki", "󱥬"),
+        ("tomo", "󱥭"),
+        ("tu", "󱥮"),
+        ("unpa", "󱥯"),
+        ("uta", "󱥰"),
+        ("utala", "󱥱"),
+        ("walo", "󱥲"),
+        ("wan", "󱥳"),
+        ("waso", "󱥴"),
+        ("wawa", "󱥵"),
+        ("weka", "󱥶"),
+        ("wile", "󱥷"),
+        ("namako", "󱥸"),
+        ("kin", "󱥹"),
+        ("oko", "󱥺"),
+        ("kipisi", "󱥻"),
+        ("leko", "󱥼"),
+        ("monsuta", "󱥽"),
+        ("tonsi", "󱥾"),
+        ("jasima", "󱥿"),
+        ("kijetesantakalu", "󱦀"),
+        ("soko", "󱦁"),
+        ("meso", "󱦂"),
+        ("epiku", "󱦃"),
+        ("kokosila", "󱦄"),
+        ("lanpan", "󱦅"),
+        ("n", "󱦆"),
+        ("misikeke", "󱦇"),
+        ("ku", "󱦈"),
+        ("pake", "󱦠"),
+        ("apeja", "󱦡"),
+        ("majuna", "󱦢"),
+        ("powe", "󱦣"),
+    ]);
+
+    let mut puncts = HashMap::new();
+    puncts.insert('[', '󱦐');
+    puncts.insert(']', '󱦑');
+    puncts.insert('^', '󱦕');
+    puncts.insert('*', '󱦖');
+    puncts.insert('(', '󱦗');
+    puncts.insert(')', '󱦘');
+    puncts.insert('{', '󱦚');
+    puncts.insert('}', '󱦛');
+    puncts.insert('.', '󱦜');
+    puncts.insert(':', '󱦝');
+    // non-UCSUR ones
+    puncts.insert('<', '「');
+    puncts.insert('>', '」');
+    puncts.insert('-', '\u{200D}'); // ZWJ
+    puncts.insert(' ', '\u{3000}'); // CJK space
+
+    Schema {candis, puncts}
+}
+
+
+pub fn emoji() -> Schema {
+    let candis = build_candis(vec![
+        ("pona", "😀")
+    ]);
+    let puncts = HashMap::new();
+    Schema {candis, puncts}
+}
+
+
+
