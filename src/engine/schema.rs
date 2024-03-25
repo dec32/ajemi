da1f4251370f4bb3a78faec69c2599a5eaf09cb0
dec32 <1064103102@qq.com>
2024-03-26 01:28:14 +0800

    Extract schemas to separate files.

--- src/engine/schema.rs
+++ src/engine/schema.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use log::error;
 use Candidate::*;
 
 /// To expain why a certain spelling is mapped to certain word(s)
+#[derive(Debug)]
 pub enum Candidate {
     /// The spelling is an exact spelling of a certain word.
     /// Meanwhile it can also be a prefix of other words.
@@ -17,6 +19,7 @@ pub enum Candidate {
 
 /// Stores the dictionary and the remapped punctuators.
 /// The dicitonary is indexed in a special way.
+#[derive(Debug)]
 pub struct Schema {
     pub candis: HashMap<String, Candidate>,
     pub alters: HashMap<String, Vec<String>>,
@@ -25,389 +28,141 @@ pub struct Schema {
     pub dquote: (char, char),
 }
 
-impl Schema {
-    fn new<const D:usize, const P:usize>(dict: [(&str, &str);D], punctuators: [(char, char);P]) -> Schema {
+
+//----------------------------------------------------------------------------
+//
+//  Load schemas from files.
+//
+//----------------------------------------------------------------------------
+
+#[derive(Clone, Copy)]
+enum Atom<'a> {
+    Text(&'a str),
+    Punct(char)
+}
+
+impl<'a> From<&'a str> for Atom<'a> {
+    fn from(str: &'a str) -> Atom<'a> {
+        use Atom::*;
+        let mut chars = str.chars();
+        let first_ch = chars.nth(0).unwrap();
+        if !first_ch.is_alphanumeric() && chars.nth(1).is_none() {
+            Punct(first_ch)
+        } else if first_ch == '#' {
+            match u32::from_str_radix(&str[1..], 16).ok().and_then(|codepoint|char::from_u32(codepoint)) {
+                Some(punct) => Punct(punct),
+                None => Text(str)
+            }
+        } else if str == "space" {
+            Punct(' ')
+        } else {
+            Text(str)
+        }
+    }
+}
+
+impl<'a> ToString for Atom<'a> {
+    fn to_string(&self) -> String {
+        use Atom::*;
+        match self {
+            Text(text) => text.to_string(),
+            Punct(punct) => {
+                let mut string = String::with_capacity(4);
+                string.push(*punct);
+                string
+            }
+        }
+    }
+}
+
+impl From<&str> for Schema {
+    fn from(value: &str) -> Schema {
+        use Atom::*;
         let mut candis = HashMap::new();
         let mut alters = HashMap::new();
-        for (spelling, word) in dict {
-            // store exact spellings -> words
-            if let Some(Exact(exact, _)) = candis.get(spelling) {
-                match alters.get_mut(exact) {
-                    None => { 
-                        alters.insert(exact.to_string(), vec![word.to_string()]); 
-                    }
-                    Some(alters) => { 
-                        alters.push(word.to_string()); 
-                    }
-                }
-                continue;
-            } else {
-                candis.insert(spelling.to_string(), Exact(word.to_string(), Vec::new()));
-            }
-            // store prefixes -> words
-            for len in 1..spelling.len() {
-                let prefix = &spelling[0..len];
-                match candis.get_mut(prefix) {
-                    None => {
-                        candis.insert(prefix.to_string(), Unique(word.to_string()));
-                    },
-                    Some(Unique(unique)) => {
-                        let mut duplicates = Vec::new();
-                        duplicates.push(unique.clone());
-                        duplicates.push(word.to_string());
-                        candis.insert(prefix.to_string(), Duplicates(duplicates));
-                    },
-                    Some(Duplicates(duplicates)) | Some(Exact(_, duplicates)) => {
-                        duplicates.push(word.to_string());
-                    }
-                }
-            }
-        }
-    
         let mut puncts = HashMap::new();
-        let squote = ('\'', '\'');
-        let dquote = ('「', '」');
-        for (punct, remapped) in punctuators {
-            puncts.insert(punct, remapped);
-        }
+        let mut squote = ('\'', '\'');
+        let mut dquote = ('"', '"');
 
+        let mut atoms = Vec::new();
+        for list in value.lines() {
+            if list.is_empty() || list.starts_with("//") {
+                continue;
+            }
+            atoms.clear();
+            atoms.extend(list.split(char::is_whitespace).filter(|str|!str.is_empty()).map(Atom::from));
+            match atoms[..] {
+                [Punct('\''), Punct(open), Punct(close)] => {
+                    squote = (open, close);
+                }
+                [Punct('"'), Punct(open), Punct(close)] => {
+                    dquote = (open, close);
+                }
+                [Punct('\''), Punct(dumb)] => {
+                    squote = (dumb, dumb);
+                }
+                [Punct('"'), Punct(dumb)] => {
+                    dquote = (dumb, dumb);
+                }
+                [Punct(punct), Punct(remapped)] => {
+                    puncts.insert(punct, remapped);
+                }
+                [Text(spelling), word, .. ] => {
+                    // store exact spelling -> word
+                    candis.insert(spelling.to_string(), Exact(word.to_string(), Vec::new()));
+                    // store prefixes -> word
+                    for len in 1..spelling.len() {
+                        let prefix = &spelling[0..len];
+                        match candis.get_mut(prefix) {
+                            None => {
+                                candis.insert(prefix.to_string(), Unique(word.to_string()));
+                            },
+                            Some(Unique(unique)) => {
+                                let mut duplicates = Vec::new();
+                                duplicates.push(unique.clone());
+                                duplicates.push(word.to_string());
+                                candis.insert(prefix.to_string(), Duplicates(duplicates));
+                            },
+                            Some(Duplicates(duplicates)) | Some(Exact(_, duplicates)) => {
+                                duplicates.push(word.to_string());
+                            }
+                        }
+                    }
+                    // store word -> alternatives
+                    let word = word.to_string();
+                    for alter in atoms.iter().skip(2) {
+                        match alters.get_mut(&word) {
+                            None => { 
+                                alters.insert(word.clone(), vec![alter.to_string()]); 
+                            }
+                            Some(alters) => { 
+                                alters.push(alter.to_string()); 
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    error!("Unrecogniable pattern: {list}");
+                    // unrecognizable pattern
+                }
+            }
+        }
         Schema {candis, alters, puncts, squote, dquote}
     }
 }
 
-
 pub fn sitelen() -> Schema {
-    Schema::new([
-        ("a", "󱤀"),      
-        ("akesi", "󱤁"),  
-        ("ala", "󱤂"),    
-        ("alasa", "󱤃"),  
-        ("ale", "󱤄"),    
-        ("anpa", "󱤅"),   
-        ("ante", "󱤆"),   
-        ("anu", "󱤇"),    
-        ("awen", "󱤈"),   
-        ("e", "󱤉"),      
-        ("en", "󱤊"),     
-        ("esun", "󱤋"),   
-        ("ijo", "󱤌"),    
-        ("ike", "󱤍"),    
-        ("ilo", "󱤎"),    
-        ("insa", "󱤏"),   
-        ("jaki", "󱤐"),   
-        ("jan", "󱤑"),    
-        ("jelo", "󱤒"),   
-        ("jo", "󱤓"),     
-        ("kala", "󱤔"),   
-        ("kalama", "󱤕"), 
-        ("kama", "󱤖"),   
-        ("kasi", "󱤗"),   
-        ("ken", "󱤘"),    
-        ("kepeken", "󱤙"),
-        ("kili", "󱤚"),   
-        ("kiwen", "󱤛"),  
-        ("ko", "󱤜"),
-        ("kon", "󱤝"),
-        ("kule", "󱤞"),
-        ("kulupu", "󱤟"),
-        ("kute", "󱤠"),
-        ("la", "󱤡"),
-        ("lape", "󱤢"),
-        ("laso", "󱤣"),
-        ("lawa", "󱤤"),
-        ("len", "󱤥"),
-        ("lete", "󱤦"),
-        ("li", "󱤧"),
-        ("lili", "󱤨"),
-        ("linja", "󱤩"),
-        ("lipu", "󱤪"),
-        ("loje", "󱤫"),
-        ("lon", "󱤬"),
-        ("luka", "󱤭"),
-        ("lukin", "󱤮"),
-        ("lupa", "󱤯"),
-        ("ma", "󱤰"),
-        ("mama", "󱤱"),
-        ("mani", "󱤲"),
-        ("meli", "󱤳"),
-        ("mi", "󱤴"),
-        ("mije", "󱤵"),
-        ("moku", "󱤶"),
-        ("moli", "󱤷"),
-        ("monsi", "󱤸"),
-        ("mu", "󱤹"),
-        ("mun", "󱤺"),
-        ("musi", "󱤻"),
-        ("mute", "󱤼"),
-        ("nanpa", "󱤽"),
-        ("nasa", "󱤾"),
-        ("nasin", "󱤿"),
-        ("nena", "󱥀"),
-        ("ni", "󱥁"),
-        ("nimi", "󱥂"),
-        ("noka", "󱥃"),
-        ("o", "󱥄"),
-        ("olin", "󱥅"),
-        ("ona", "󱥆"),
-        ("open", "󱥇"),
-        ("pakala", "󱥈"),
-        ("pali", "󱥉"),
-        ("palisa", "󱥊"),
-        ("pan", "󱥋"),
-        ("pana", "󱥌"),
-        ("pi", "󱥍"),
-        ("pilin", "󱥎"),
-        ("pimeja", "󱥏"),
-        ("pini", "󱥐"),
-        ("pipi", "󱥑"),
-        ("poka", "󱥒"),
-        ("poki", "󱥓"),
-        ("pona", "󱥔"),
-        ("pu", "󱥕"),
-        ("sama", "󱥖"),
-        ("seli", "󱥗"),
-        ("selo", "󱥘"),
-        ("seme", "󱥙"),
-        ("sewi", "󱥚"),
-        ("sijelo", "󱥛"),
-        ("sike", "󱥜"),
-        ("sin", "󱥝"),
-        ("sina", "󱥞"),
-        ("sinpin", "󱥟"),
-        ("sitelen", "󱥠"),
-        ("sona", "󱥡"),
-        ("soweli", "󱥢"),
-        ("suli", "󱥣"),
-        ("suno", "󱥤"),
-        ("supa", "󱥥"),
-        ("suwi", "󱥦"),
-        ("tan", "󱥧"),
-        ("taso", "󱥨"),
-        ("tawa", "󱥩"),
-        ("telo", "󱥪"),
-        ("tenpo", "󱥫"),
-        ("toki", "󱥬"),
-        ("tomo", "󱥭"),
-        ("tu", "󱥮"),
-        ("unpa", "󱥯"),
-        ("uta", "󱥰"),
-        ("utala", "󱥱"),
-        ("walo", "󱥲"),
-        ("wan", "󱥳"),
-        ("waso", "󱥴"),
-        ("wawa", "󱥵"),
-        ("weka", "󱥶"),
-        ("wile", "󱥷"),
-        ("namako", "󱥸"),
-        ("kin", "󱥹"),
-        ("oko", "󱥺"),
-        ("kipisi", "󱥻"),
-        ("leko", "󱥼"),
-        ("monsuta", "󱥽"),
-        ("tonsi", "󱥾"),
-        ("jasima", "󱥿"),
-        ("kijetesantakalu", "󱦀"),
-        ("soko", "󱦁"),
-        ("meso", "󱦂"),
-        ("epiku", "󱦃"),
-        ("kokosila", "󱦄"),
-        ("lanpan", "󱦅"),
-        ("n", "󱦆"),
-        ("misikeke", "󱦇"),
-        ("ku", "󱦈"),
-        ("pake", "󱦠"),
-        ("apeja", "󱦡"),
-        ("majuna", "󱦢"),
-        ("powe", "󱦣"),
-    ],[
-        ('[', '󱦐'),
-        (']', '󱦑'),
-        ('^', '󱦕'),
-        ('*', '󱦖'),
-        ('(', '󱦗'),
-        (')', '󱦘'),
-        ('{', '󱦚'),
-        ('}', '󱦛'),
-        ('.', '󱦜'),
-        (':', '󱦝'),
-        ('<', '「'),
-        ('>', '」'),
-        ('-', '\u{200D}'), // ZWJ
-        (' ', '\u{3000}'), // CJK space
-    ])
+    Schema::from(include_str!("../../res/schema/sitelen.schema"))
 }
 
 pub fn emoji() -> Schema {
-    Schema::new([
-        ("a", "🅰️"),
-        ("akesi", "🦎"),
-        ("akesi", "🐸"),
-        ("ala", "❌"),
-        ("alasa", "🏹"),
-        ("ale", "🌌"),
-        ("anpa", "🧎"),
-        ("anpa", "🙇"),
-        ("ante", "🔀"),
-        ("anu", "🤷"),
-        ("awen", "⚓"),
-        ("e", "⏩"),
-        ("en", "🤝"),
-        ("esun", "🛒"),
-        ("ijo", "🐚"),
-        ("ike", "😔"),
-        ("ike", "👎"),
-        ("ilo", "🔦"),
-        ("insa", "🗳️"),
-        ("jaki", "💩"),
-        ("jan", "🧑"),
-        ("jelo", "🍋"),
-        ("jo", "👜"),
-        ("kala", "🐟"),
-        ("kala", "🐙"),
-        ("kalama", "👏"),
-        ("kama", "🛬"),
-        ("kasi", "🌱"),
-        ("ken", "💪"),
-        ("kepeken", "✍️"),
-        ("kili", "🍎"),
-        ("kiwen", "💎"),
-        ("ko", "🍦"),
-        ("kon", "💨"),
-        ("kule", "🌈"),
-        ("kulupu", "👥"),
-        ("kute", "👂"),
-        ("la", "ℹ️"),
-        ("la", "💁"),
-        ("lape", "😴"),
-        ("laso", "☘️"),
-        ("lawa", "👑"),
-        ("len", "🧣"),
-        ("lete", "❄️"),
-        ("li", "▶️"),
-        ("lili", "🐁"),
-        ("linja", "🧶"),
-        ("lipu", "🍁"),
-        ("loje", "👅"),
-        ("lon", "⏺️"),
-        ("lon", "✅"),
-        ("lon", "🫴"),
-        ("luka", "🖐️"),
-        ("lukin", "👀"),
-        ("lupa", "🚪"),
-        ("ma", "🏝️"),
-        ("mama", "🍼"),
-        ("mani", "🐮"),
-        ("meli", "👩"),
-        ("meli", "🚺"),
-        ("mi", "👇"),
-        ("mi", "🅿️"),
-        ("mije", "👨"),
-        ("mije", "🚹"),
-        ("moku", "🍜"),
-        ("moli", "😵"),
-        ("monsi", "🍑"),
-        ("mu", "🐽"),
-        ("mun", "🌙"),
-        ("musi", "🎭"),
-        ("mute", "👐"),
-        ("nanpa", "#️⃣"),
-        ("nasa", "🌀"),
-        ("nasin", "🛤️"),
-        ("nena", "🗻"),
-        ("ni", "⬇️"),
-        ("ni", "⬆️"),
-        ("ni", "⬅️"),
-        ("ni", "➡️"),
-        ("nimi", "📛"),
-        ("noka", "🦵"),
-        ("o", "🅾️"),
-        ("olin", "💕"),
-        ("ona", "👈"),
-        ("ona", "♋️"),
-        ("open", "🎬"),
-        ("pakala", "💥"),
-        ("pali", "🏗️"),
-        ("palisa", "📏"),
-        ("pan", "🍞"),
-        ("pana", "🙌"),
-        ("pi", "📎"),
-        ("pilin", "❤️"),
-        ("pimeja", "🎱"),
-        ("pini", "🏁"),
-        ("pini", "🛑"),
-        ("pipi", "🐛"),
-        ("poka", "👯"),
-        ("poki", "📦"),
-        ("pona", "😌"),
-        ("pona", "👍"),
-        ("pu", "🧘"),
-        ("sama", "⚖️"),
-        ("seli", "🔥"),
-        ("selo", "🍌"),
-        ("seme", "❓"),
-        ("sewi", "☁️"),
-        ("sijelo", "🧍"),
-        ("sike", "⭕"),
-        ("sin", "✨"),
-        ("sina", "👆"),
-        ("sina", "6️⃣"),
-        ("sinpin", "🗿"),
-        ("sitelen", "🎨"),
-        ("sitelen", "🖼️"),
-        ("sona", "🧠"),
-        ("soweli", "🦔"),
-        ("suli", "🐘"),
-        ("suno", "☀️"),
-        ("supa", "🛏️"),
-        ("suwi", "🍬"),
-        ("tan", "↩️"),
-        ("taso", "🚦"),
-        ("taso", "🚥"),
-        ("tawa", "🛫"),
-        ("telo", "💧"),
-        ("tenpo", "🕒"),
-        ("toki", "💬"),
-        ("tomo", "🏠"),
-        ("tu", "⏸️"),
-        ("unpa", "🍆"),
-        ("uta", "👄"),
-        ("utala", "⚔️"),
-        ("utala", "🆚"),
-        ("walo", "🐑"),
-        ("wan", "1️⃣"),
-        ("waso", "🐦"),
-        ("wawa", "⚡"),
-        ("weka", "🆑"),
-        ("wile", "🙏"),
-        ("wile", "🧲"),
-        ("epiku", "😁"),
-        ("jasima", "🪞"),
-        ("jasima", "🪩"),
-        ("kijetesantakalu", "🦡"),
-        ("kijetesantakalu", "🦝"),
-        ("kin", "*️⃣"),
-        ("kipisi", "✂️"),
-        ("kokosila", "🐊"),
-        ("ku", "🔬"),
-        ("lanpan", "🤳"),
-        ("leko", "🧱"),
-        ("meso", "😑"),
-        ("misikeke", "💊"),
-        ("monsuta", "👻"),
-        ("n", "🆖"),
-        ("namako", "🌶️"),
-        ("oko", "👁️"),
-        ("soko", "🍄"),
-        ("tonsi", "⚧️"),
-        ("majuna", "🪷"),
-        ("majuna", "💾"),
-        ("majuna", "🧓"),
-        ("su", "🧙"),
-        ("su", "🧵"),
-    ], [
-        ('[', '\u{1F58C}'),
-        (']', '\u{1F58C}'),
-    ])
+    Schema::from(include_str!("../../res/schema/emoji.schema"))
 }
 
-
+#[test]
+fn test_schema_parsing() {
+    let sitelen_schema = include_str!("../../res/schema/sitelen.schema");
+    let emoji_schema = include_str!("../../res/schema/emoji.schema");
+    println!("{:?}", Schema::from(sitelen_schema));
+    println!("{:?}", Schema::from(emoji_schema));
+}
